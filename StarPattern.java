02328d678983a8d3e566f2c672793501e5c1d82a
medhini1122 <medhinipeddapalli@gmail.com>
2025-08-14 10:50:48 +0530

    Add files via upload

---
+++ StarPattern.java
@@ -0,0 +1,12 @@
+public class StarPattern {
+    public static void main(String[] args) {
+        int rows = 5;  // Number of rows in the pattern
+
+        for (int i = 1; i <= rows; i++) {     
+            for (int j = 1; j <= i; j++) {    
+                System.out.print("* ");
+            }
+            System.out.println();              
+        }
+    }
+}
